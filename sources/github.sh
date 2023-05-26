a4275fb62f9362a7a767e3b12142f5080d03f76e
gfdgd_xi <3025613752@qq.com>
2023-05-26 23:15:55 +0800

    更新source

---
+++ sources/github.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+mkdir -p /tmp/gfdgd-xi-sources
+wget -P /tmp/gfdgd-xi-sources http://deb.wine.wine-runner.gfdgdxi.top/gpg.asc
+wget -P /tmp/gfdgd-xi-sources http://deb.wine.wine-runner.gfdgdxi.top/sources/github.list
+gpg --dearmor /tmp/gfdgd-xi-sources/gpg.asc
+#sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys FD6EEA1F20CD4B27
+sudo cp -v /tmp/gfdgd-xi-sources/gpg.asc.gpg /etc/apt/trusted.gpg.d/gfdgdxi-list-winehq.gpg
+sudo cp -v /tmp/gfdgd-xi-sources/github.list /etc/apt/sources.list.d/gfdgdxi-list-winehq.list
+sudo apt update
